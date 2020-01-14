20987debde72a6a761434170d79ee1103271089f
Richard Joseph <richjoseph1129@gmail.com>
2020-01-13 20:19:32 -0500

    Merge and Insert analysis complete

---
+++ insert_time.py
@@ -0,0 +1,33 @@
+import random
+import time
+
+def insert_sort(array):
+    for i in range(1, len(array)):
+        curr = array[i]
+        pos = i
+
+        while pos > 0 and array[pos - 1] > curr:
+            array[pos] = array[pos - 1]
+            pos -= 1
+
+        array[pos] = curr
+
+def random_num_gen():
+    new_num = input("Hi how many numbers would you like to generate? ")
+    random_list = [random.randrange(0, 10001, 1) for i in range(int(new_num))]
+
+    return random_list
+
+
+def main():
+    rand_num_list = random_num_gen()
+    start = time.perf_counter()
+    insert_sort(rand_num_list)
+    elapsed = (time.perf_counter() - start)
+    print("Size of n: ", len(rand_num_list))
+    print("Elapsed Time: ", elapsed)
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ merge_time.py
@@ -0,0 +1,53 @@
+import random
+import time
+
+def merge_sort(array):
+    if len(array) > 1:
+        mid_num = len(array) // 2  # Find the middle of the list "//" used so that integer is returned
+        left = array[:mid_num]  # Gets the elements to the left of middle number
+        right = array[mid_num:]  # Holds elements to the right of the middle number including middle number
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i = 0
+        j = 0
+        k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                array[k] = left[i]
+                i += 1
+            else:
+                array[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            array[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            array[k] = right[j]
+            j += 1
+            k += 1
+
+
+def random_num_gen():
+    new_num = input("Hi how many numbers would you like to generate? ")
+    random_list = [random.randrange(0, 10001, 1) for i in range(int(new_num))]
+
+    return random_list
+
+
+def main():
+    rand_num_list = random_num_gen()
+    start = time.perf_counter()
+    merge_sort(rand_num_list)
+    elapsed = (time.perf_counter() - start)
+    print("Size of n: ", len(rand_num_list))
+    print("Elapsed Time: ", elapsed)
+
+if __name__ == "__main__":
+    main()
